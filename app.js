9766b8ad8659231df8b4fcf3081db690e4816505
Bhavneet Singh <bhavneetsingh@192.168.1.5>
2024-03-27 11:39:22 +0530

    L3 mid

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const heading = React.createElement("h1", {}, "This is my heading.")
-root.render(heading)
+// const heading = React.createElement("h1", {}, "This is my heading.")
+// root.render(heading)
+
+const h = <h1>JSX element</h1>;
+
+const HeadingComponent = () => <h1>Functional component</h1> ;
+
+root.render(<HeadingComponent />);
+ 
